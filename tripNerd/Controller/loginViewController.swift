199fa36e36c53bbfdac406720091b9b34a0ecca8
Eric Torigian <eric@torigian.com>
2018-04-12 11:28:54 -0400

    sign up a user working

--- tripNerd/Controller/loginViewController.swift
+++ tripNerd/Controller/loginViewController.swift
@@ -14,6 +14,7 @@ class loginViewController: UIViewController {
     var loginMode = true
     
     //MARK - IBOutlets
+    @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
@@ -22,6 +23,9 @@ class loginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if loginMode {
+            usernameTextField.isHidden = true
+        }
  
         }
  
@@ -29,8 +33,28 @@ class loginViewController: UIViewController {
     @IBAction func loginButtonPressed(_ sender: Any) {
         if loginMode {
             //log a user in
+            
         } else {
             //sign a ueser up
+            if usernameTextField.text != nil {
+                if emailTextField.text != nil {
+                    if passwordTextField.text != nil  {
+                        let user = PFUser()
+                        
+                        user.username = usernameTextField.text
+                        user.email = emailTextField.text
+                        user.password = passwordTextField.text
+                        
+                        user.signUpInBackground { (success, error) in
+                            if error == nil {
+                                self.showAlert(title: "Success", message: "You have successfully signed up for Trip Nerd")
+                            } else {
+                                self.showAlert(title: "Error", message: (error?.localizedDescription)!)
+                            }
+                        }
+                    }
+                }
+            }
             
             
             
@@ -39,16 +63,28 @@ class loginViewController: UIViewController {
     
     @IBAction func signInButtonPressed(_ sender: Any) {
         if loginMode == true {
+            usernameTextField.isHidden = false
             loginMode = false
             loginButton.setTitle("Sign Up", for: .normal)
             signupQuestionLabel.text = "Click here to cancel signup"
             signupButton.setTitle("Log In", for: .normal)
         } else {
+            usernameTextField.isHidden = true
             loginMode = true
             loginButton.setTitle("Log In", for: .normal)
             signupQuestionLabel.text = "Don't have an account on Trip Nerd?"
             signupButton.setTitle("Sign Up", for: .normal)
         }
+    }
+    
+    //MARK - support functions
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) in
+            self.dismiss(animated: true, completion: nil)
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
